a6bec7e82c4569666905a5ac14221c1dbc2ac3e8
novakvova <novakvova@gmail.com>
2020-03-17 09:29:04 +0200

    Add button and toast

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.testapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClick(View view) {
+        EditText editText = findViewById(R.id.txtName);
+        TextView textView = findViewById(R.id.vtInfo);
+        String name=editText.getText().toString();
+        textView.setText(name);
+
+        Toast.makeText(this,
+                name,Toast.LENGTH_LONG).show();
     }
 }
