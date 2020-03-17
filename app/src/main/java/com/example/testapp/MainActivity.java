eab36da45c13fd231042446e1000cd2e98081b6e
novakvova <novakvova@gmail.com>
2020-03-17 10:08:25 +0200

    Add Register page input

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -2,18 +2,37 @@ package com.example.testapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnRegister = findViewById(R.id.btnGoRegister);
+
+        // создаем обработчик нажатия
+        View.OnClickListener oclBtnRegister = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Меняем текст в TextView (tvOut)
+//                Toast.makeText(MainActivity.this,
+//                        "Перехід на реєстрацію",Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        };
+
+        // присвоим обработчик кнопке OK (btnOk)
+        btnRegister.setOnClickListener(oclBtnRegister);
     }
 
     public void onButtonClick(View view) {
---
+++ app/src/main/java/com/example/testapp/RegisterActivity.java
@@ -0,0 +1,20 @@
+package com.example.testapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+    public void onGotoMainhome(View view) {
+        Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
