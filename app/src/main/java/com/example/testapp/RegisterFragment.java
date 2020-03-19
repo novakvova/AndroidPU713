381f736eff680af394f3b60c2c6b76a50e68ae9a
Home <Home@DESKTOP-KA21DG9>
2020-03-19 11:45:18 +0200

    add register fragment

--- app/src/main/java/com/example/testapp/RegisterFragment.java
+++ app/src/main/java/com/example/testapp/RegisterFragment.java
@@ -2,13 +2,26 @@ package com.example.testapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegisterFragment extends Fragment {
+
+    public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 
     public RegisterFragment() {
         // Required empty public constructor
@@ -31,6 +44,81 @@ public class RegisterFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
+
+        final TextInputLayout passwordTextInput = view.findViewById(R.id.password_text_input);
+        final TextInputEditText passwordEditText = view.findViewById(R.id.password_edit_text);
+
+        MaterialButton nextButton = view.findViewById(R.id.next_button);
+
+        // Set an error if the password is less than 8 characters.
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isEmailValid(view);
+                if (!isPasswordValid(passwordEditText.getText())) {
+                    passwordTextInput.setError("Вкажіть пароль");
+                } else {
+                    passwordTextInput.setError(null); // Clear the error
+                    ((NavigationHost) getActivity()).navigateTo(new NewsFragment(), false); // Navigate to the next Fragment
+                }
+            }
+        });
+
+        // Clear the error once more than 8 characters are typed.
+        passwordEditText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if (isPasswordValid(passwordEditText.getText())) {
+                    passwordTextInput.setError(null); //Clear the error
+                    //return true;
+                }
+                return false;
+            }
+        });
+        return view;
+    }
+
+    private boolean isPasswordValid(@Nullable Editable text) {
+        return text != null && text.length() >= 8;
+    }
+
+    private boolean isNameValid(@Nullable Editable text) {
+        return text != null && text.length() >= 1;
+    }
+
+    private boolean isEmailValid(@Nullable View view) {
+
+
+        final TextInputLayout emailTextInput = view.findViewById(R.id.email_text_input);
+        final TextInputEditText emailEditText = view.findViewById(R.id.email_edit_text);
+        Editable text = emailEditText.getText();
+        try {
+
+            if (text != null) {
+//                RegisterFragment.validate(text.toString());
+
+//        if (matcher.find()){
+//            emailTextInput.setError("Вкажіть пароль");
+//            return false;
+//        }
+//        else {
+//            emailTextInput.setError(null);
+//            return true;
+//        }
+            }
+        } catch (Exception ex) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isPhoneValid(@Nullable Editable text) {
+        return text != null && text.length() >= 8;
+    }
+
+    public static boolean validate(String emailStr) {
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(emailStr);
+        return matcher.find();
     }
 }
