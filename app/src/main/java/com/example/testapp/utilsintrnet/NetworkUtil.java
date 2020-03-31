430a79cabb8039c1932d424faec2b4abe513489f
novakvova <novakvova@gmail.com>
2020-03-31 09:22:31 +0300

    Validation network

--- app/src/main/java/com/example/testapp/utilsintrnet/NetworkUtil.java
+++ app/src/main/java/com/example/testapp/utilsintrnet/NetworkUtil.java
@@ -3,7 +3,6 @@ package com.example.testapp.utilsintrnet;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.widget.Toast;
 
 public class NetworkUtil {
     public static boolean isOnline(Context context) {
